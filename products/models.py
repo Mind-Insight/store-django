14e8ffde2cb5be61a7c9b0931e44e8c1aa091c00
Timofey <nsk.timka2077@gmail.com>
2023-02-17 22:37:06 +0200

    created model ProductCategory and Products in models.py and made first migrations

--- products/models.py
+++ products/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class ProductCategory(models.Model):
+    name = models.CharField(max_length=128, unique=True)
+    description = models.TextField(null=True, blank=True)
+
+class Products(models.Model):
+    name = models.CharField(max_length=256)
+    description = models.TextField(null=True, blank=True)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    quantity = models.PositiveIntegerField(default=0)
+    image = models.ImageField(upload_to='products_images')
+    category = models.ForeignKey(to=ProductCategory, on_delete=models.CASCADE)
