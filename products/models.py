8293c6aa3e50b925e47b685d51f04eb4d22b0e62
Timofey <nsk.timka2077@gmail.com>
2023-02-18 20:50:35 +0200

    Revert "edited context in views.py and edited products.html with jinja"
    This reverts commit 43ba73b9ac4b46fb8ac0d662021c8677ebbd741d.

--- products/models.py
+++ products/models.py
@@ -12,10 +12,7 @@ class ProductCategory(models.Model):
 class Products(models.Model):
     name = models.CharField(max_length=256)
     description = models.TextField(null=True, blank=True)
-    price = models.DecimalField(max_digits=7, decimal_places=2)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
     quantity = models.PositiveIntegerField(default=0)
     image = models.ImageField(upload_to='products_images')
     category = models.ForeignKey(to=ProductCategory, on_delete=models.CASCADE)
-    
-    def __str__(self):
-        return f"Product: {self.name} | Category: {self.category.name}"
--- products/views.py
+++ products/views.py
@@ -1,16 +1,9 @@
 from django.shortcuts import render, HttpResponse
 
-from products.models import ProductCategory, Products
-
 
 def index(request):
     return render(request, 'products/index.html', {'title': "Testing template"})
 
 
 def products(request):
-    context = {
-        'title': 'Store - Каталог',
-        'products': Products.objects.all(),
-        'categories': ProductCategory.objects.all(),
-    }
-    return render(request, 'products/products.html', context)
+    return render(request, 'products/products.html')
