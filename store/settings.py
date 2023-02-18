631973cc4589c4ff34456f4299b3b487dbd70556
Timofey <nsk.timka2077@gmail.com>
2023-02-18 22:17:42 +0200

    created folder media with all pictures for products and created folder fixtures with all data for db as json

--- store/settings.py
+++ store/settings.py
@@ -122,6 +122,8 @@ STATICFILES_DIRS = (
     BASE_DIR / 'static',
 )
 
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
--- store/urls.py
+++ store/urls.py
@@ -2,9 +2,15 @@ from django.contrib import admin
 from django.urls import path
 from products.views import index, products
 
+from django.conf.urls.static import static
+from django.conf import settings
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', index, name='index'),
     path('products/', products, name='products')
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
