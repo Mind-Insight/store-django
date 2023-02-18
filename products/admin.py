162972de60a3dfba086253cd40850c3a525f782b
Timofey <nsk.timka2077@gmail.com>
2023-02-18 16:14:44 +0200

    registered Products and ProductCategory to admin url

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
 
-# Register your models here.
+from products.models import ProductCategory, Products
+admin.site.register(ProductCategory)
+admin.site.register(Products)
--- products/models.py
+++ products/models.py
@@ -1,8 +1,13 @@
 from django.db import models
 
+
 class ProductCategory(models.Model):
     name = models.CharField(max_length=128, unique=True)
     description = models.TextField(null=True, blank=True)
+    
+    def __str__(self):
+        return self.name
+
 
 class Products(models.Model):
     name = models.CharField(max_length=256)
