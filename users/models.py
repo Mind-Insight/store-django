f58d86a16731700550aaff653b08b266d2085bb7
Timofey <nsk.timka2077@gmail.com>
2023-02-19 22:45:25 +0200

    extended model User and made migrations. Registered this model

--- users/models.py
+++ users/models.py
@@ -1,2 +1,6 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
+
+class User(AbstractUser):
+    image = models.ImageField(upload_to='users_images', null=True, blank=True)
