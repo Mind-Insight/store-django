6a787c45e32fa96001029234d10044d62f35f0fd
Timofey <nsk.timka2077@gmail.com>
2023-02-19 19:20:47 +0200

    edited fixtures

--- store/urls.py
+++ store/urls.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from products.views import index, products
 
 from django.conf.urls.static import static
