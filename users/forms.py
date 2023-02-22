ed3cf7972d39523c62c502d8479cbe3ddb6a7a96
Timofey <nsk.timka2077@gmail.com>
2023-02-22 21:58:10 +0200

    created form for user's profile. Created view for profile. Added ability to set custom avatar

--- users/forms.py
+++ users/forms.py
@@ -1,15 +1,13 @@
-from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm, UserChangeForm
 from django import forms
 from users.models import User
 
 
 class UserLoginForm(AuthenticationForm):
     username = forms.CharField(widget=forms.TextInput(attrs={
-        'class': 'form-control py-4', 'placeholder': 'Введите имя пользователя'
-    }))
+        'class': 'form-control py-4', 'placeholder': 'Введите имя пользователя'}))
     password = forms.CharField(widget=forms.PasswordInput(attrs={
-        'class': 'form-control py-4', 'placeholder': "Введите пароль"
-    }))
+        'class': 'form-control py-4', 'placeholder': "Введите пароль"}))
     class Meta:
         model = User
         fileds = ['username', 'password']
@@ -35,3 +33,20 @@ class UserRegistationForm(UserCreationForm):
     class Meta:
         model = User
         fields = ('first_name', 'last_name', 'username', 'email', 'password1', 'password2')
+
+
+class UserProfileForm(UserChangeForm):
+    first_name = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control py-4'}))
+
+    last_name = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control py-4'}))
+
+    image = forms.ImageField(widget=forms.FileInput(attrs={'class': 'custom-file-input'}))
+
+    username = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control py-4', 'readonly': True}))
+
+    email = forms.CharField(widget=forms.EmailInput(attrs={'class': 'form-control py-4', 'readonly': True}))
+
+
+    class Meta:
+        model = User
+        fields = ('first_name', 'last_name', 'image', 'username', 'email')
