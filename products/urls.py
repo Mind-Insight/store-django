4c0cded04fe6fa14f33bbe5c32c96c9dd5eff193
Timofey <nsk.timka2077@gmail.com>
2023-02-25 22:23:01 +0200

    add ability to put product in cart

--- products/urls.py
+++ products/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path, include
-from products.views import products
+from products.views import products, cart_add
 
 
 app_name = 'products'
 urlpatterns = [
-    path('', products, name='index')
+    path('', products, name='index'),
+    path('baskets/add/<int:product_id>/', cart_add, name='cart_add')
 ]
--- products/views.py
+++ products/views.py
@@ -1,6 +1,8 @@
+from django.http import HttpResponseRedirect
 from django.shortcuts import render, HttpResponse
 
-from products.models import ProductCategory, Products
+from products.models import ProductCategory, Products, Cart
+from users.models import User
 
 
 def index(request):
@@ -14,3 +16,16 @@ def products(request):
         'categories': ProductCategory.objects.all()
     }
     return render(request, 'products/products.html', context)
+
+
+def cart_add(request, product_id):
+    product = Products.objects.get(id=product_id)
+    baskets = Cart.objects.filter(user=request.user, product=product)
+
+    if not baskets.exists():
+        Cart.objects.create(user=request.user, product=product, quantity=1)
+    else:
+        basket = baskets.first()
+        basket.quantity += 1
+        basket.save()
+    return HttpResponseRedirect(request.META['HTTP_REFERER'])
